5c7478c363d89d71a3a0414ecb13e45af8520bbe
Gayan <gayanf@gmail.com>
2021-04-11 19:34:53 +0530

    Containerization Complete

---
+++ WineWeb/Models/Wine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace WineWeb.Models
+{
+    public class Wine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Type { get; set; }
+        public string Year { get; set; }
+        public string Age { get; set; }
+        public string Varietal { get; set; }
+        public string Size { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
